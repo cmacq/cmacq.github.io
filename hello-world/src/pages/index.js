0c6c86d2e00681c9be4a00759e561fc096ce027e
Chad McCue <chadanita@gmail.com>
2020-09-16 12:32:44 -0500

    updated content for cole demo

--- hello-world/src/pages/index.js
+++ hello-world/src/pages/index.js
@@ -4,7 +4,8 @@ export default function Home() {
   return (
     <div style={{ color: `purple` }}>
       <h1>Hello Gatsby!</h1>
-      <p>What a world.</p>
+      <p>What a world. I like to add stuff. add more stuff.</p>
+      <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Filler text</a>
       <img src="https://source.unsplash.com/random/400x200" alt="" />
     </div>
   )
